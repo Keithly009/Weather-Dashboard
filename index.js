71f3d70c347712de94cd468d7888ed9d531f3066
Keithly009 <113906981+Keithly009@users.noreply.github.com>
2022-11-07 16:22:29 -0800

    furthered the foundation of html, removed some js with intention to replace

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
+// 
+// All of the elements
 var API_KEY = '500315c8473f75376f0ad6eb7eed421c'
 
+input.addEventListener('keyup', function(event) {
+    if (event.key === 'Enter') {
+        createWeatherDisplay(event.target.value); }
+})
+
 function getGeoLocation(query, limit = 5) { 
-    return fetch('http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_KEY}')
-    
+    return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_KEY}`) 
 }
 
+// Will get the current weather from the locations 
 function getCurrentWeather({arguments}) { 
-    return fetch('https://api.openweathermap.org/data/3.0/onecall?lat=${arguments.lat}&lon=${arguments.lon}&units=${arguments.units}&appid=${API_KEY}')
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${arguments.lat}&lon=${arguments.lon}&units=${'imperial,'}&appid=${API_KEY}`)
 }
 
 
 // Using a function that returns a promise, which is to fetch the api geolocator 
-getGeoLocation('Long Beach')
+function createWeatherDisplay(cityLocation) { 
+    return getGeoLocation(cityLocation)
 .then(function(response) {
-    return response.json
-})
+    return response.json 
+}) 
+}
 .then(data=> {
+    if (data.length == 0) {
+        document.body.appendChild('We could not find ${location}')
+    } else if 
     getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
     .then(weatherResponse => weatherResponse.json())
     .then(weatherData => { 
-        document.body.textContent = JSONstringify(weatherData, null, 2)
+        var weatherPicture = document.createElement('img')
+        weatherPicture.src = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+        var currentWeatherStatement = document.createElement('p')
+        currentWeatherStatement.textcontent = `${weatherData.weather[0].main}: the weather is currently ${weatherData.weather[0].description}`
+        
+        document.body.appendChild(currentWeatherStatement)
     })
     .catch(error => { 
         document.body.textContent = error.message
@@ -28,4 +45,5 @@ getGeoLocation('Long Beach')
 .catch(error => { 
     document.body.textContent = error.message
 })
+}
 
